f0a9fbb161345f96860a2bed8b49bd8069858378
Alex Jansen <alexjansen210@gmail.com>
2024-06-30 13:11:50 -0700

    Tweak eslint settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,9 @@
 /* eslint-env node */
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    extends: ['eslint:recommended',
+      'plugin:@typescript-eslint/strict',
+      'plugin:@typescript-eslint/stylistic',
+    ],
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint'],
     root: true,
