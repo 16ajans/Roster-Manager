0d0c1bbee7105ca62c5c06f4fd445d8ce8049e71
Alex Jansen <alexjansen210@gmail.com>
2024-07-29 21:35:27 -0700

    Create embed builder

--- src/drivers/bot.ts
+++ src/drivers/bot.ts
@@ -1,4 +1,4 @@
-import { Client, Events, GatewayIntentBits, Guild, GuildResolvable, UserResolvable } from 'discord.js'
+import { Client, EmbedBuilder, Events, GatewayIntentBits, Guild, GuildResolvable, UserResolvable } from 'discord.js'
 import 'dotenv/config'
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] })
@@ -26,8 +26,28 @@ export function fetchUser (userSnowflake: UserResolvable) {
     return client.users.fetch(userSnowflake)
 }
 
-export function sendDM (userSnowflake: UserResolvable, message: string) {
-    return client.users.send(userSnowflake, message)
+export async function sendDM (userSnowflake: UserResolvable, playerSnowflake: UserResolvable, accepted: boolean, adminSnowflake: UserResolvable, reason?: string) {
+    return client.users.send(userSnowflake, { embeds: [await buildVerifNotifEmbed(playerSnowflake, accepted, adminSnowflake, reason)] })
 }
 
 client.login(process.env.DISCORD_BOT_TOKEN)
+
+async function buildVerifNotifEmbed(playerSnowflake: UserResolvable, accepted: boolean, adminSnowflake: UserResolvable, reason?: string) {
+    const embed = new EmbedBuilder()
+        .setColor(0x703893)
+        .setAuthor({ name: "CVRE Roster Integration", url: "https://cvre.app/", iconURL: "https://cvre.app/images/CVRE.png"})
+        .setFooter({ text: "This is an automated message. Do not reply."})
+    const admin = await fetchGuildMember(adminSnowflake)
+    const player = await fetchGuildMember(playerSnowflake)
+    if (accepted) {
+        embed.setTitle(`${player.displayName}'s verification has been approved!`)
+    } else {
+        embed
+            .setTitle(`${player.displayName}'s verification has been rejected.`)
+            .setDescription(`Please contact ${admin.displayName} (<@${adminSnowflake}>) or another CVRE admin for further information.`)
+        if (reason) {
+            embed.addFields({ name: "Reason:", value: reason })
+        }
+    }
+    return embed.setTimestamp()
+}
