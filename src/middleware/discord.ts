f570f7c9d7340666978410ae1c5bb2b4d4f72461
Alex Jansen <alexjansen210@gmail.com>
2024-09-15 16:41:24 -0700

    Hydrate users/managers

--- src/middleware/discord.ts
+++ src/middleware/discord.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { fetchGuildMember, searchUniqueGuildMember } from "../drivers/bot";
 import { noUpload } from "../drivers/fs";
-import { Player } from "@prisma/client";
+import { Player, User } from "@prisma/client";
 import { GuildMember } from "discord.js";
 import { prisma } from "../drivers/db";
 
@@ -32,15 +32,20 @@ router.post('/member/search', noUpload, async (req, res) => {
     }
 })
 
+interface HydratedUser extends Omit<User, 'discord'> {
+    discord: string | GuildMember
+}
+
 interface HydratedPlayer extends Omit<Player, 'discord'> {
     discord: string | GuildMember
 }
 
-export const hydratePlayer = async function (player: HydratedPlayer) {
-    player.discord = await fetchGuildMember(player.discord)
-    return player
+
+export const hydrateOne = async function (one: HydratedPlayer | HydratedUser) {
+    one.discord = await fetchGuildMember(one.discord)
+    return one
 }
 
-export const hydratePlayers = async function (players: HydratedPlayer[]) {
-    return await Promise.all(players.map(player => hydratePlayer(player)))
+export const hydrateMany = async function (many: HydratedPlayer[] | HydratedUser[]) {
+    return await Promise.all(many.map(one => hydrateOne(one)))
 } 
