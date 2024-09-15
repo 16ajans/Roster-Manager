4cf2b4cd1978c8a87489f9641f6aebad80e464b0
Alex Jansen <alexjansen210@gmail.com>
2024-09-15 13:44:15 -0700

    Fix verification from pre-refactor state

--- src/routes/verify.ts
+++ src/routes/verify.ts
@@ -1,6 +1,9 @@
 import express from 'express'
 import path from 'path'
 import { dirRoot } from '../drivers/fs'
+import { prisma } from '../drivers/db'
+import { sendVerifDM } from '../drivers/bot'
+import { State } from '@prisma/client'
 
 export const router = express.Router()
 
@@ -19,3 +22,66 @@ router.use('/docs', express.static(path.join(dirRoot, 'verifications'), {
     },  
     maxAge: '1d'
   }))
+
+  router
+    .get('/list', async (req, res) => {
+        const query: State[] = []
+        if (req.query.REJECTED === "on") {
+            query.push(State.REJECTED)
+        }
+        if (req.query.AWAITING === "on") {
+            query.push(State.AWAITING)
+        }
+        if (req.query.REVIEW === "on") {
+            query.push(State.REVIEW)
+        }
+        if (req.query.ACCEPTED === "on") {
+            query.push(State.ACCEPTED)
+        }
+        const players = await prisma.player.findMany({
+            where: {
+                status: {
+                    in: query
+                }
+            },
+            include: {
+                manager: true
+            }
+        })
+        res.render('fragments/verify/list', {
+            players
+        })
+    })
+    .put('/:playerID/:newState', express.urlencoded({ extended: true }), async (req, res) => {
+        let param: State = State.REJECTED
+        if (req.params.newState === "ACCEPTED") {
+            param = State.ACCEPTED
+        }
+        const player = await prisma.player.update({
+            where: {
+                id: req.params.playerID
+            },
+            data: {
+                status: param
+            },
+            include: {
+                manager: true
+            }
+        })
+        res.render('fragments/verify/changed', {
+            player
+        })
+        if (param == State.ACCEPTED) {
+            sendVerifDM(player.manager.discord, player.discord, true)
+        } else {
+          sendVerifDM(player.manager.discord, player.discord, false, req.session.user?.discord, req.body.reason)
+        }
+    })
+    .delete('/:playerID', async (req, res) => {
+        await prisma.player.delete({
+            where: {
+                id: req.params.playerID
+            }
+        })
+        res.send("<p>Player registration deleted.</p>")
+    })
