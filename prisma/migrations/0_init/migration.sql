4409e97ab862342d1738245c8017bf818fe42a56
Alex Jansen <alexjansen210@gmail.com>
2024-09-18 17:43:53 -0700

    Baseline production db & update prisma

--- prisma/migrations/0_init/migration.sql
+++ prisma/migrations/0_init/migration.sql
@@ -3,14 +3,14 @@ CREATE TYPE "State" AS ENUM ('REJECTED', 'AWAITING', 'REVIEW', 'ACCEPTED');
 
 -- CreateTable
 CREATE TABLE "Division" (
-    "id" UUID NOT NULL,
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "open" BOOLEAN NOT NULL,
-    "min_players" INTEGER NOT NULL,
-    "max_players" INTEGER NOT NULL,
+    "min_players" INTEGER,
+    "max_players" INTEGER,
     "name" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "manager_role" TEXT NOT NULL,
-    "player_role" TEXT NOT NULL,
+    "description" TEXT,
+    "manager_role" TEXT,
+    "player_role" TEXT,
     "adminId" UUID NOT NULL,
 
     CONSTRAINT "Division_pkey" PRIMARY KEY ("id")
@@ -18,21 +18,22 @@ CREATE TABLE "Division" (
 
 -- CreateTable
 CREATE TABLE "Team" (
-    "id" UUID NOT NULL,
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "name" TEXT NOT NULL,
-    "locale" TEXT NOT NULL,
+    "locale" TEXT,
     "divisionId" UUID NOT NULL,
     "managerId" UUID NOT NULL,
+    "logo" TEXT,
 
     CONSTRAINT "Team_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Assignment" (
-    "id" UUID NOT NULL,
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "alt_tag" TEXT NOT NULL,
-    "scoresaber" TEXT NOT NULL,
-    "status" "State" NOT NULL,
+    "scoresaber" TEXT,
+    "status" "State" NOT NULL DEFAULT 'REVIEW',
     "playerId" UUID NOT NULL,
     "teamId" UUID NOT NULL,
 
@@ -41,11 +42,12 @@ CREATE TABLE "Assignment" (
 
 -- CreateTable
 CREATE TABLE "Player" (
-    "id" UUID NOT NULL,
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "name" TEXT NOT NULL,
     "school" TEXT NOT NULL,
     "discord" TEXT NOT NULL,
-    "status" "State" NOT NULL,
+    "doc" TEXT,
+    "status" "State" NOT NULL DEFAULT 'AWAITING',
     "managerId" UUID NOT NULL,
 
     CONSTRAINT "Player_pkey" PRIMARY KEY ("id")
@@ -53,13 +55,33 @@ CREATE TABLE "Player" (
 
 -- CreateTable
 CREATE TABLE "User" (
-    "id" UUID NOT NULL,
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "discord" TEXT NOT NULL,
-    "avatar" TEXT NOT NULL,
-    "auth" TEXT NOT NULL,
+    "avatar" TEXT,
+    "auth" TEXT,
+    "roles" TEXT[],
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" TEXT NOT NULL,
+    "sid" TEXT NOT NULL,
+    "data" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Player_discord_key" ON "Player"("discord");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_discord_key" ON "User"("discord");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Session_sid_key" ON "Session"("sid");
 
 -- AddForeignKey
 ALTER TABLE "Division" ADD CONSTRAINT "Division_adminId_fkey" FOREIGN KEY ("adminId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -71,10 +93,11 @@ ALTER TABLE "Team" ADD CONSTRAINT "Team_divisionId_fkey" FOREIGN KEY ("divisionI
 ALTER TABLE "Team" ADD CONSTRAINT "Team_managerId_fkey" FOREIGN KEY ("managerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Assignment" ADD CONSTRAINT "Assignment_playerId_fkey" FOREIGN KEY ("playerId") REFERENCES "Player"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Assignment" ADD CONSTRAINT "Assignment_playerId_fkey" FOREIGN KEY ("playerId") REFERENCES "Player"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Assignment" ADD CONSTRAINT "Assignment_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "Team"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Assignment" ADD CONSTRAINT "Assignment_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "Team"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Player" ADD CONSTRAINT "Player_managerId_fkey" FOREIGN KEY ("managerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
