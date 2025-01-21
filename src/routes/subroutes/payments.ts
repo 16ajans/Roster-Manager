bf005eda4470f41322ba1c8b603853b093d6e7e0
Alex Jansen <alexjansen210@gmail.com>
2025-01-20 21:44:41 -0800

    Implement payments management features: add list and update routes, create payment card mixin, and enhance admin portal navigation (again)

--- src/routes/subroutes/payments.ts
+++ src/routes/subroutes/payments.ts
@@ -1,4 +1,7 @@
 import express from 'express'
+import { prisma } from '../../drivers/db'
+import { State } from '@prisma/client'
+import { hydrateOne } from '../../middleware/discord'
 
 export const router = express.Router()
 
@@ -8,3 +11,54 @@ router.get('/', async (req, res) => {
         user: req.session.user
     })
 })
+
+router
+    .get('/list', async (req, res) => {
+        const query: State[] = []
+        if (req.query.UNPAID === "on") {
+            query.push(State.AWAITING)
+        }
+        if (req.query.PAID === "on") {
+            query.push(State.ACCEPTED)
+        }
+        const teams = await prisma.team.findMany({
+            where: {
+                paid: {
+                    in: query
+                }
+            },
+            include: {
+                division: true,
+                manager: true
+            }
+        })
+        // await hydrateMany(players)
+        for (const team of teams) {
+            await hydrateOne(team.manager)
+        }
+        res.render('fragments/payments/list', {
+            teams
+        })
+    })
+    .put('/:teamID/:newState', express.urlencoded({ extended: true }), async (req, res) => {
+        let param: State = State.AWAITING
+        if (req.params.newState === "PAID") {
+            param = State.ACCEPTED
+        }
+        const team = await prisma.team.update({
+            where: {
+                id: req.params.teamID
+            },
+            data: {
+                paid: param
+            },
+            include: {
+                manager: true,
+                division: true
+            }
+        })
+        await hydrateOne(team.manager)
+        res.render('fragments/payments/changed', {
+            team
+        })
+    })
